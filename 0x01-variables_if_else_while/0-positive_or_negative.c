6c3bf913e6fc6e9eeea529d838f85f9233f69c51
maiyo008 <tonymaiyo008@gmail.com>
2022-09-09 17:32:34 +0000

    Modify to conform betty coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/*
+ * Random number generated everytime the code is run
+ * 
+ * Check if a number is even, odd or zero if neither
+**/
 
-int main (void)
+int main(void)
 {
 	int n;
 
@@ -10,11 +15,11 @@ int main (void)
 	n = rand() -RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	else
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 
 	return (0);
 }
