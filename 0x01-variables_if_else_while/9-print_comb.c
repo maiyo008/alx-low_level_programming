aa93e1ff6da9aa6c74349256ead3865bd41df745
maiyo008 <tonymaiyo008@gmail.com>
2022-09-09 21:03:18 +0000

    Print all possible combinations of single digits separated with a comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print all possible combinations of single digits\
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
