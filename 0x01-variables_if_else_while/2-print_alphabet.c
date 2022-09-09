e47a782b2d518b9691dfd58aea7a60f11ac110d8
maiyo008 <tonymaiyo008@gmail.com>
2022-09-09 18:55:35 +0000

    Print all alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Print all aphabet in lowercase
+ * Printf not allowed
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+	return (0);
+}
