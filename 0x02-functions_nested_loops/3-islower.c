5038d9b362b5567adff1faf4eeef12c48a22226d
maiyo008 <tonymaiyo008@gmail.com>
2022-09-13 18:40:55 +0000

    Betty style corrections

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,13 @@
 /**
  * _islower - Entry to the function
  * check for lower or upper cases
+ * Function takes an argument c 
  * Return: 0(Success)
  */
 int _islower(int c)
 {
 	int a;
+
 	if ((c >= 97) && (c <= 122))
 		a = 1;
 	else
