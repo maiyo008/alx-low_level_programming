b61052559b8cd2f4ffd119c45443092bd86dfabe
maiyo008 <tonymaiyo008@gmail.com>
2022-09-14 05:07:58 +0000

    Betty style corrections

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - Check function
  * @c: An input character
- * Description: functions returns 1 if the character 
+ * Description:Find if a character
  * is a letter, lowercase or uppercase
  * Return: 1 or 0 in otherwise
  */
