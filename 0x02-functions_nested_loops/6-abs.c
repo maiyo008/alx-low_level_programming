e34d5409e4d21d364603bac2184b18d5bdbdc807
maiyo008 <tonymaiyo008@gmail.com>
2022-09-14 11:33:49 +0000

    Betty style corrections

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,12 +4,12 @@
  * _abs - Entry point
  * @n: An input number
  * Description:Return the absolute value of an integer
- * Return: Return value of n 
+ * Return:Return value of n 
  */
 int _abs(int n)
 {
 	if (n >= 0)
-		return n;
+		return (n);
 	else
 		return (n * -1);
 }
