78978150cb2bd540984b1fea3127bf7c3b3e3ebd
maiyo008 <tonymaiyo008@gmail.com>
2022-09-14 11:30:28 +0000

    Print an absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: An input number
+ * Description:Return the absolute value of an integer
+ * Return: Return value of n 
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return n;
+	else
+		return (n * -1);
+}
