1a96487eb091cae395e0e3a316ee70bba2caf37e
maiyo008 <tonymaiyo008@gmail.com>
2022-09-14 11:06:38 +0000

    Betty style corrections

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,8 @@
  * print_sign - Entry point
  * @n: An input number
  * Description:This functions print sign of a number
- * Return: 1 if number is positive, 0 if number is 0, or -1 if number is negative
+ * Return: 1 if number is positive,
+ * 0 if number is 0, or -1 if number is negative
  */
 int print_sign(int n)
 {
