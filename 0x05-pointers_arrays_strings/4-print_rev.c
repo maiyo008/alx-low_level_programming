55ea2d0e713898cd084cb1baf92081a4f2b7590f
maiyo008 <tonymaiyo008@gmail.com>
2022-09-19 09:29:38 +0000

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - Entry point
+ * @s:A pointer input to a character
+ * Description:Print a string in reverse
+ * Return:Nothing
+ */
+void print_rev(char *s)
+{
+	int l;
+
+	l = strlen(s);
+	while (l >= 0)
+	{
+		_putchar(s[l]);
+		l--;
+	}
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -6,4 +6,5 @@
 |Task 1 |Write a function that swaps the values of two integers|
 |Task 2 |Write a function that returns the length of a string|
 |Task 3 |Write a function that prints a string, followed by a new line, to stdout|
+|Task 4 |Write a function that prints a string, in reverse, followed by a newline|
 
