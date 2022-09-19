de7cfe03117c0b0b5d243041c7d8f065aa7d01c8
maiyo008 <tonymaiyo008@gmail.com>
2022-09-19 08:03:34 +0000

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @n:A pointer input int
+ * Description:Takes a pointer to int n and updates it to n = 98
+ * Return:Returns nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
