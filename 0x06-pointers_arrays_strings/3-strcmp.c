61e33caf600f252098045a81e7bb66eef1317660
maiyo008 <tonymaiyo008@gmail.com>
2022-09-22 08:29:04 +0000

    Output corrections to task 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,25 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int s1len = 0, s2len = 0;
-	int val;
+	int i = 0;
 
-	while (*s1)
-	{
-		s1len++;
-		s1++;
-	}
-	while (*s2)
-	{
-		s2len++;
-		s2++;
-	}
-	if (s1len < s2len)
-		val = s1len - s2len;
-	else if (s1len == s2len)
-		val = 0;
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+		i++;
+
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
 	else
-		val = s1len - s2len;
-	return (val);
+		return (0);
 }
-
