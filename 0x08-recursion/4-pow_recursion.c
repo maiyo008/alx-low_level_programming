94fe0e9e358c4fa5d641dcc16b172fb7fcdc8ff5
maiyo008 <tonymaiyo008@gmail.com>
2022-09-28 18:22:42 +0000

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return value of x raised to the power of y
+ * @x: base number input
+ * @y: raised value number input
+ * Return: x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -6,4 +6,5 @@
 |Task 1  |Write a function that prints a string in reverse|
 |Task 2  |Write a function that returns the length of a string|
 |Task 3  |Write a function that returns the factorial of a number|
+|Task 4  |Write a function that returns the value of x raised to the power of y|
 
