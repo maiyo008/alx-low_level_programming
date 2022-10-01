d9e23567b7dc86ea621cab108d10da24a63cd6ee
maiyo008 <tonymaiyo008@gmail.com>
2022-10-01 05:47:43 +0000

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that multiplies two numbers
+ * @argc: The arguments' counter
+ * @argv: The arguments' values
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int a, b, c;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = a * b;
+		printf("%d\n", c);
+		return (0);
+	}
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -5,4 +5,4 @@
 |Task 0  |Write a program that prints its name, followed by a new line|
 |Task 1  |Write a program that prints the number of arguments passed onto it|
 |Task 2  |Write a program that prints all the arguments it receives|
-
+|Task 3  |Write a program that multiplies two numbers|
