2304ac3305e5e2c2adc2600d56a012fa29f95924
maiyo008 <tonymaiyo008@gmail.com>
2022-10-06 21:32:00 +0000

    Task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - A function that creates an array of integers
+ * array_range - A function that creates an array of integers
+ * @max: max number to start
+ * @max: max number to start
+ */
+int *array_range(int min, int max)
+{
+	int *new_array;
+	int diff, i;
+
+	if (min > max)
+		return (NULL);
+	diff = max - min;
+	new_array = malloc((diff + 1) * sizeof(int));
+	if (new_array == NULL)
+		return (NULL);
+	for (i = 0; i <= diff; i++)
+		new_array[i] = min++;
+	return (new_array);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -5,4 +5,4 @@
 |Task 0  |Write a function that allocates memory using malloc|
 |Task 1  |Write a function that concatenates two strings|
 |Task 2  |Write a function that allocates memory foe ab array using malloc|
-
+|Task 3  |Write a function that creates an array of itegers|
