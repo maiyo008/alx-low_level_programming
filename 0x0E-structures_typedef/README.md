400e0e6c629ba00c80301c87a3eb24770cc3bb7c
maiyo008 <tonymaiyo008@gmail.com>
2022-10-11 06:46:39 +0000

    Task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+# 0x0E. C - Structures, typedef
+---
+|Task no |Tasks	|
+|--------|------|
+|Task 0  |Define a new type struct|
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - A new structure with the following types
+ * @name: First member of type char
+ * @age: Second member of type float
+ * @owner: Third member of type char
+ * Description: The structure has three data members
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
