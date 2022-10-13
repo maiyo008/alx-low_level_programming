6ac1910cc52cf6e90db8de67c0c991c6407a1a76
maiyo008 <tonymaiyo008@gmail.com>
2022-10-13 22:00:55 +0000

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function to add two ints
+ * @a:first number input
+ * @b:second number input
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to subtract two numbers
+ * @a: first number input
+ * @b: second number input
+ * Return: difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function to multiply two numbers
+ * @a: first number input]
+ * @b: second number input
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function to divide two numbers
+ * @a: first number input
+ * @b: second number input
+ * Return: quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function to find modulus of two numbers
+ * @a: first number input
+ * @b: second number input
+ * Return: modulus of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -5,4 +5,13 @@
 |Task 0 |Write a function that prints a name|
 |Task 1 |Write a function that executes a function given as parameter on each element of an array|
 |Task 2 |Write a function that searches for an integer|
+|Task 3 |Write a program that:
+* uses > calc num1 operator num2 
+* Assume num1 and num2 are integers
+* Operator is one of + - * / %
+* prints result of operation followed by newline
+* Assume result will be stored in an int
+* if number of arguments is wrong, print Error, followed by newline and exit with status 98
+* if the operator is non of the above, print Error, followed by newline and exit with status 99
+* if user tries to divide (/ or %) by 0, print Error, followed by a newline, and exit with status 100|
 
