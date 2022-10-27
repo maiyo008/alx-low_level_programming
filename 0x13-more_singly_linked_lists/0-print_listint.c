60c92b076f5e1160d4255b309864955ab6a3861e
maiyo008 <tonymaiyo008@gmail.com>
2022-10-27 04:26:11 +0000

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - print all elements of the list
+ * @h: pointer to the head 
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+# 0x13. C - More singly linked lists
+---
+|Tasks no |Tasks	|
+|---------|-------------|
+|Task 0   |Write a function that prints all the elements of a list|
+
