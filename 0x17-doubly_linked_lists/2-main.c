faa4253792f47a281f8a0aabbf2cd7e1e41a1de2
maiyo008 <tonymaiyo008@gmail.com>
2022-12-09 02:56:28 +0000

    Task 2

---
+++ 0x17-doubly_linked_lists/2-main.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+	dlistint_t *head;
+
+	head = NULL;
+	add_dnodeint(&head, 0);
+	add_dnodeint(&head, 1);
+	add_dnodeint(&head, 2);
+	add_dnodeint(&head, 3);
+	print_dlistint(head);
+	return (EXIT_SUCCESS);
+}
