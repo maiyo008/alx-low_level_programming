63c6a82ac39cab5c16210ecb7f75b4ddfa7ab230
maiyo008 <tonymaiyo008@gmail.com>
2022-12-09 03:13:07 +0000

    Task 3

---
+++ 0x17-doubly_linked_lists/3-main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+	dlistint_t *head;
+
+	head = NULL;
+	add_dnodeint_end(&head, 0);
+	add_dnodeint_end(&head, 1);
+	add_dnodeint_end(&head, 98);
+	print_dlistint(head);
+	return (EXIT_SUCCESS);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -5,4 +5,5 @@
 |Task 0  |Write a function that prints all the elements of a list|
 |Task 1  |Write a function that returns the numnber of elements in a linked list|
 |Task 2  |Write a function that adds a new node at the beginning of a list|
+|Task 3  |Write a function that adds a new node at the end of a list|
 
