804750160323210f9af7e62c7d14286e3d59850f
maiyo008 <tonymaiyo008@gmail.com>
2022-12-14 10:28:28 +0000

    Task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - function to add two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: return the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function to subtract two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: return the difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - function to multiply two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: return the product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - function to divide two numbers
+ * @a: first digit
+ * @b: second digit
+ * Return: returns a quotient of a floating type
+ */
+float div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - function to compute modulus
+ * @a: first digit
+ * @b: second digit
+ * Return: returns a modulus
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
